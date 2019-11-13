137924d5f0e7fdb67af572f1b8d88f273638dca4
josecolin4 <jose.colin@iut.local>
2019-11-13 09:39:00 +0100

    fix #3.2 Moteur de recherche sur status et username

---
+++ all_users3.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>TD1</title>		
+	</head>
+	
+	<body>
+		
+		<!-- saisie des informations -->
+		<form method="post" action="all_users3.php" ID="formRecherche">
+			<select name="actif">
+				<option>Active account</option>
+				<option>Waiting for account validation</option>
+			</select>
+			
+			<input type="text" name="nom" />
+
+			<input type="submit" />
+		</form>
+	
+	
+		<table>
+		<?php
+		if (isset($_POST['actif']) && isset($_POST['nom'])) {			
+			$host = 'localhost';
+			$db   = 'my_activities';
+			$user = 'root';
+			$pass = 'root';
+			$charset = 'utf8mb4';
+			$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+			$options = [
+			PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+			PDO::ATTR_EMULATE_PREPARES   => false,
+			];
+			try {
+				$pdo = new PDO($dsn, $user, $pass, $options);
+			} catch (PDOException $e) {
+				throw new PDOException($e->getMessage(), (int)$e->getCode());
+			} 
+			$stmt = $pdo->prepare('SELECT * FROM users AS u JOIN status AS s ON u.status_id=s.id WHERE s.name = ? AND u.username LIKE ? ORDER BY username');
+			$stmt->execute([$_POST['actif'],$_POST['nom'].'%']);
+			while ($row = $stmt->fetch()) {
+				echo '<tr>';
+				echo '<td>'.$row['id'].'</td>';
+				echo '<td>'.$row['username'].'</td>';
+				echo '<td>'.$row['email'].'</td>';
+				echo '<td>'.$row['name'].'</td>';
+				echo '</tr>';
+			}
+		}
+		?>
+		</table>
+	</body>
+	
+</html>
